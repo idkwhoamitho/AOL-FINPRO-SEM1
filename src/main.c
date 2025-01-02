f225787c5e40eaf67f303ccaefdb6e88bd0575bc
Mike <mike99wazoski@gmail.com>
2025-01-03 00:06:24 +0700

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include "data_processing.h"
+#include "menu.h"
+#include "fileIO.h"
+
 int getInventoryById(struct Inventory* Inventory, int id){
     int i = 0;
     int found;
@@ -15,4 +20,42 @@ int getInventoryById(struct Inventory* Inventory, int id){
     
     return index;
     
+}
+
+void menu() {
+    int choose;
+    
+     while(1){
+        printf("INVENTORY SYSTEM\n");
+        printf("INVENTORY DATA MAIN MENU\n\n");
+        printf("What do you want to do?\n");
+        printf("1. Insert Record\n");
+        printf("2. Show Record\n");
+        printf("3. Select Data\n");
+        printf("4. Delete Record\n");
+        printf("5. Update Record\n");
+        printf("0. Exit\n\n");
+        printf("Your choice: ");
+        scanf("%d", &choose);
+        
+        if(choose == 0){
+        	system("cls || clear"); break;
+		} else if(choose == 1){
+            addRecordMenu();
+		} else if(choose == 2){
+            
+		} else if(choose == 3){
+            
+		} else if(choose == 4){
+            
+		} else if(choose == 5){
+            
+		} else if(choose == 6){
+            
+		} else if(choose == 7){
+            
+		}
+
+    
+    }
 }
